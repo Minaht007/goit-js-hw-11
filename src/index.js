6dc3589ad6fb20c86c68216583f1d57c5bb052f7
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-01-26 12:48:13 +0200

    start files

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+axios.get('/users')
+  .then(res => {
+    console.log(res.data);
+  });
+import Notiflix from 'notiflix';
+  
+const form = document.querySelector('.search-form');
+console.log(form)
+
+
+
+
+
+
+
+{/* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */}
